c205067010ebe665312464298b2906726b76faee
Altair Dias <89491054+altairfd@users.noreply.github.com>
2022-10-15 18:23:21 -0300

    Add files via upload

---
+++ backend/src/main/java/com/devsupeior/projcatalog/ProjcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.devsupeior.projcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjcatalogApplication.class, args);
+	}
+
+}
